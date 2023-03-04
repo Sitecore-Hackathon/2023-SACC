80acd72b657f08ead442fe9203a1496f76e8b991
Adrián Steinsleger <adrian.steinsleger@verndale.com>
2023-03-04 12:15:29 -0500

    Fix GPT3 class

--- src/lib/GPT3.ts
+++ src/lib/GPT3.ts
@@ -1,4 +1,4 @@
-import * as openai from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 /**
  * GPT3 class
@@ -11,26 +11,27 @@ import * as openai from 'openai';
  * const prompt = "What is the meaning of life?";
  * const response = await gpt3.query(prompt);
  * console.log(response); // "The meaning of life is..."
- * 
+ *
  */
 
 export class GPT3 {
-  private readonly API_KEY: string;
+  private readonly configuration: Configuration;
+  private readonly openai: OpenAIApi;
 
   constructor(apiKey: string) {
-    this.API_KEY = apiKey;
-    openai.apiKey = apiKey;
+    this.configuration = new Configuration({ apiKey: apiKey });
+    this.openai = new OpenAIApi(this.configuration);
   }
 
-  async query(prompt: string, model: string = "text-davinci-002", maxTokens: number = 2000): Promise<string> {
-    const params: openai.CompletionParams = {
+  async query(prompt: string): Promise<string | undefined> {
+    const response = await this.openai.createCompletion({
+      model: 'text-davinci-003',
       prompt: prompt,
-      model: model,
-      maxTokens: maxTokens,
+      max_tokens: 2000,
+      stop: ['\n'],
       n: 1,
-      stop: "\n",
-    };
-    const response = await openai.completions.create(params);
-    return response.choices[0].text.trim();
+    });
+
+    return response.data.choices[0].text;
   }
 }
